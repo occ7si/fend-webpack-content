de2b93cbea27f2101b739799023b3193d5afd32a
caterineocampo <cocampo@gmx.de>
2020-04-24 14:28:01 +0200

    chore: Set dependencioes to webpack, changed port - added webpack dependencies - changed server port to 3030

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(3030, function () {
+    console.log('Example app listening on port 3030!')
 })
 
 app.get('/test', function (req, res) {
